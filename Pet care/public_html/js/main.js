1580f3829be7f345d8a0770f948f2afff0cd5c2a
frontend <frontend@10.70.1.163>
2017-03-07 16:02:28 +0100

    rad na casu

--- Pet care/public_html/js/main.js
+++ Pet care/public_html/js/main.js
@@ -4,9 +4,27 @@ $(document).ready(function(){
 });
 
 
+/*TEAM SECTION SLIDER*/
 
-
-
+$('.my-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:3
+        },
+        992:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+});
 
 
 
